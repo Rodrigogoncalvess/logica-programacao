aea4041666f5deb46abfecca3b58c7732ee23f51
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-16 19:02:17 -0300

    Calculando porcentagem

---
+++ src/PorcetangemSalario.java
@@ -0,0 +1,12 @@
+public class PorcetangemSalario {
+    public static void main(String[] args) {
+
+        double salario = 2750.00;
+        int porcentagem = 20;
+        double calcularPorcentagem = (salario / 100) * porcentagem;
+
+        System.out.println("Calculando a porcentagem: " + calcularPorcentagem);
+    }
+
+
+}
