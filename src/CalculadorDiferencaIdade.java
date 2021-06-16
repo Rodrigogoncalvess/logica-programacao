e25a9693b62dd77934ba1e596d1119d80c460459
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-16 18:19:05 -0300

    Canculando variáveis

---
+++ src/CalculadorDiferencaIdade.java
@@ -0,0 +1,11 @@
+public class CalculadorDiferencaIdade {
+    public static void main(String[] args) {
+
+        int primeiraIdade = 76;
+        int segundaIdade = 39;
+        int diferencaIdade = primeiraIdade - segundaIdade;
+
+        System.out.println("Diferença entre as idades: " + diferencaIdade);
+//        System.out.println("Diferença entre as idades: "+(primeiraIdade - segundaIdade));
+    }
+}
