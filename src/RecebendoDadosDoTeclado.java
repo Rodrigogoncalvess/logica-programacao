7a234e5810c358c794797b912bd90c64a57927d5
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-17 13:48:23 -0300

    Lendo dados do teclado - Scanner

---
+++ src/RecebendoDadosDoTeclado.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RecebendoDadosDoTeclado {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+        System.out.println("Digite sua idade: ");
+        System.out.println("Nome digitado foi: " + nome);
+        System.out.println("Idade digitado foi: " + idade);
+
+
+    }
+}
