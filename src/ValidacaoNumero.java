b712719f6d97c8cf0cdebe8d50c9a9a935537ab7
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-17 12:19:02 -0300

    Validação de número par ou ímpar

---
+++ src/ValidacaoNumero.java
@@ -0,0 +1,20 @@
+/*
+Dado determinado numero inteiro
+Imprima se ele é ímpar ou par
+Resto da divisão por 2 ser for par sempre vai ser 0 senão vai ser ímpar
+ */
+
+public class ValidacaoNumero {
+    public static void main(String[] args) {
+
+        int nummero = 34;
+
+        if (nummero % 2 == 0) {
+            System.out.println("Numero Par");
+        } else {
+            System.out.println("Numero Impar");
+        }
+
+    }
+
+}
