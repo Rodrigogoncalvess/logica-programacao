e7a25f1b94939cdcd95f4fc392b1e6851fedb9df
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-17 09:37:55 -0300

    Reutilizando variáveis

--- src/PorcetangemSalario.java
+++ src/PorcetangemSalario.java
@@ -1,8 +1,8 @@
 public class PorcetangemSalario {
     public static void main(String[] args) {
 
-        double salario = 2750.00;
-        int porcentagem = 20;
+        double salario = 17.00;
+        int porcentagem = 22;
         double calcularPorcentagem = (salario / 100) * porcentagem;
 
         System.out.println("Calculando a porcentagem: " + calcularPorcentagem);
---
+++ src/ReutilizandoVariaveis.java
@@ -0,0 +1,13 @@
+public class ReutilizandoVariaveis {
+    public static void main(String[] args) {
+
+        double salario = 17.00;
+        double resultado = salario * 0.5;
+
+        System.out.println("Calculando a porcentagem: " + resultado);
+        resultado = salario * 0.19;
+        System.out.println("Calculando a porcentagem: " + resultado);
+    }
+
+
+}
