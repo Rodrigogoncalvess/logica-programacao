6ab73e41a5cb3e174efa36846ff2114354d27057
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-17 10:00:06 -0300

    Testando variáveis

---
+++ src/TestandoVariaveis.java
@@ -0,0 +1,13 @@
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+
+        char masculino = 'M';
+        char feminino = 'F';
+        String masculino2 = "M";
+        String feminino2 = "F";
+        boolean condicao = true;
+        System.out.println(condicao);
+    }
+
+
+}
