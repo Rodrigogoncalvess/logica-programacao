de2c9cb4a2efd1937e15c09bd0e3b58968799c51
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-17 14:02:57 -0300

    Lendo dados do teclado - utilizando Scanner

--- src/TorneioDeNatacao.java
+++ src/TorneioDeNatacao.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 public class TorneioDeNatacao {
     public static void main(String[] args) {
 
-        String nome = "Rodrigo ";
-        int idade = 16;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do participante:");
+        String nome = scanner.next();
+        System.out.println("Digite a idade do participante:");
+        int idade = scanner.nextInt();
+
+
 
         if (idade <= 10) {
-            System.out.println(nome + "Participará da categoria Infantil");
+            System.out.println(nome  + " Participará da categoria Infantil");
         } else if (idade >= 11 && idade <= 15) {
-            System.out.println(nome + "Participará da categoria Juvenil");
+            System.out.println(nome  + " Participará da categoria Juvenil");
         } else if (idade >= 16 && idade <= 19) {
-            System.out.println(nome + "Participará da categoria Pré-adulto");
+            System.out.println(nome  + " Participará da categoria Pré-adulto");
         } else {
             System.out.println(nome + "Participará da categoria Adulto");
         }
+
     }
 
 
