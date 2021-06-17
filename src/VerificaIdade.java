6f78e873ec58d452e80b397bf1e671aface49f27
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-17 11:10:48 -0300

    Exercício IF e ELSE

---
+++ src/VerificaIdade.java
@@ -0,0 +1,12 @@
+public class VerificaIdade {
+    public static void main(String[] args) {
+
+        int idade = 19;
+
+        if (idade > 18) {
+            System.out.println("Adulto");
+        } else {
+            System.out.println("Ainda não é adulto");
+        }
+    }
+}
