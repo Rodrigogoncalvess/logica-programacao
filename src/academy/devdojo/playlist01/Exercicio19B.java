ed05e3bc13707ce699ec8c7882367fc7127778c2
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-19 17:33:44 -0300

    Exercicio 19B

--- src/academy/devdojo/playlist01/Exercicio19B.java
+++ src/academy/devdojo/playlist01/Exercicio19B.java
@@ -1,36 +1,36 @@
 package academy.devdojo.playlist01;
 /*
 Em um circuito elétrico temos as seguintes informações:
-275 V e 10 A, calcule a resistência elétrica, utilizando a fórmula abaixo:
-𝑅 = 𝑈
-𝐼
+275 Ω e 1 A, calcule a tensão elétrica, utilizando a fórmula abaixo:
+𝑈 = 𝑅 · 𝐼
 Onde:
-R = Resistência em Ohms
 U = Tensão em Volts
+R = Resistência em Ohms
 I = Corrente em Amperes
 BÔNUS: Altere o programa acima e permita que o usuário digite os valores
-para Volts e Amperes.
+para Ohms e Amperes.
  */
+
 import java.util.Scanner;
 
-public class Exercicio19A {
+public class Exercicio19B {
     public static void main(String[] args) {
 
-        float ohms;
         float volts;
+        float ohms;
         float amperes;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe a tensão em volts: ");
-        volts = scanner.nextFloat();
+        System.out.println("Informe a resistência em Ohms: ");
+        ohms = scanner.nextFloat();
 
         System.out.println("Informe a corrente em Amperes: ");
         amperes = scanner.nextFloat();
 
-        ohms = volts / amperes;
+        volts = ohms * amperes;
 
 
-        System.out.println("Resultado da resitência em Ohms: " + ohms);
+        System.out.println("Resultado da tensão em Volts: " + volts+" V");
 
 
     }
