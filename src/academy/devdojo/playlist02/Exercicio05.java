e1f131ad31ed7cd9172105398e7d165dbdb3acf5
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-21 08:11:38 -0300

    Exercício 05

--- src/academy/devdojo/playlist02/Exercicio05.java
+++ src/academy/devdojo/playlist02/Exercicio05.java
@@ -1,46 +1,28 @@
 package academy.devdojo.playlist02;
 /*
-Desenvolva um rotina que leia as notas de quatro bimestres de um aluno
-(N1, N2, N3 e N4), e informe o status do aluno baseado na tabela abaixo:
-Média Status
-Se a média for menor que 50 REPROVADO
-Se a média for igual a 50 RECUPERAÇÃO
-Se a média for maior que 50 APROVADO
+Desenvolva um programa que execute a leitura de um valor numérico inteiro,
+caso o valor esteja entre 1 e 9 apresentar a mensagem “Dentro do valor
+permitido”e caso contrário apresentar a mensagem “Fora do Valor
+Permitido”.
  */
 
 import java.util.Scanner;
 
-public class Exercicio01 {
+public class Exercicio05 {
     public static void main(String[] args) {
 
-        int n1, n2, n3, n4;
-        int media;
+        int valor = 0;
 
         Scanner ent = new Scanner(System.in);
 
-        System.out.println("Digite a 1ª nota do aluno");
-        n1 = ent.nextInt();
 
-        System.out.println("Digite a 2ª nota do aluno");
-        n2 = ent.nextInt();
+        System.out.println("Infome um valor:");
+        valor = ent.nextInt();
 
-        System.out.println("Digite a 3ª nota do aluno");
-        n3 = ent.nextInt();
-
-        System.out.println("Digite a 4ª nota do aluno");
-        n4 = ent.nextInt();
-
-        media = (n1 + n2 + n3 + n4) / 4;
-        System.out.println("A média do aluno é: " + media);
-
-
-        if (media < 50) {
-            System.out.println("REPROVADO");
-        } else if (media == 50) {
-            System.out.println("RECUPERAÇÃO");
+        if (valor > 0 && valor <= 9) {
+            System.out.println("Dentro do valor!");
         } else {
-            System.out.println("APROVADO");
+            System.out.println("Fora do valor!");
         }
-
     }
 }
