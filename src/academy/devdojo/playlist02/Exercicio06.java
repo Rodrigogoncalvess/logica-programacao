ab773caeadb2f3a4f501b8f11c666842dde1eb35
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-21 08:11:49 -0300

    Exercício 06

--- src/academy/devdojo/playlist02/Exercicio06.java
+++ src/academy/devdojo/playlist02/Exercicio06.java
@@ -1,45 +1,28 @@
 package academy.devdojo.playlist02;
 /*
-Desenvolva um rotina que leia as notas de quatro bimestres de um aluno
-(N1, N2, N3 e N4), e informe o status do aluno baseado na tabela abaixo:
-Média Status
-Se a média for menor que 50 REPROVADO
-Se a média for igual a 50 RECUPERAÇÃO
-Se a média for maior que 50 APROVADO
+Leia um número inteiro qualquer e multiplique por 2, apresentando o
+resultado da multiplicação somente se o resultado for maior ou igual a 30.DO
  */
 
 import java.util.Scanner;
 
-public class Exercicio01 {
+public class Exercicio06 {
     public static void main(String[] args) {
 
-        int n1, n2, n3, n4;
-        int media;
+        int n1;
+        int resultado;
 
         Scanner ent = new Scanner(System.in);
 
-        System.out.println("Digite a 1ª nota do aluno");
+
+        System.out.println("Digite um número:");
         n1 = ent.nextInt();
+        resultado = n1*2;
 
-        System.out.println("Digite a 2ª nota do aluno");
-        n2 = ent.nextInt();
-
-        System.out.println("Digite a 3ª nota do aluno");
-        n3 = ent.nextInt();
-
-        System.out.println("Digite a 4ª nota do aluno");
-        n4 = ent.nextInt();
-
-        media = (n1 + n2 + n3 + n4) / 4;
-        System.out.println("A média do aluno é: " + media);
-
-
-        if (media < 50) {
-            System.out.println("REPROVADO");
-        } else if (media == 50) {
-            System.out.println("RECUPERAÇÃO");
-        } else {
-            System.out.println("APROVADO");
+        if(resultado>=30){
+            System.out.println("Resultado foi maior ou igual 30: "+resultado);
+        }else {
+            System.out.println("Resultado foi inferior ao esperado!");
         }
 
     }
