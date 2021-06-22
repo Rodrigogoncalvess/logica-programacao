5bb08016262b86a4513857a4efc0f0441aa39e7c
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-22 18:52:44 -0300

    Exercício 04

--- src/academy/devdojo/playlist03/Exercicio04.java
+++ src/academy/devdojo/playlist03/Exercicio04.java
@@ -1,19 +1,17 @@
 package academy.devdojo.playlist03;
 
 /*
-Elaborar um programa que apresente o somatório dos números na faixa de 1 até
-500.
+Elaborar uma rotina que apresente todos os números pares na faixa de 0 até 100.
  */
-public class Exercicio03 {
+public class Exercicio04 {
     public static void main(String[] args) {
 
-        int soma = 0;
 
-        System.out.print("A somatório do número 1 até 500 é: ");
-        for (int i = 1; i <= 500; i++)
-            soma += i;
-
-        System.out.println(soma);
+        System.out.println("Números pares na faxia 1 à 100");
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
     }
 }
 
