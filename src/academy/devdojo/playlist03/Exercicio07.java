422a98dd98a9bb657ad874ff4d65eb3b650fb045
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-22 18:53:34 -0300

    Exercício 07

--- src/academy/devdojo/playlist03/Exercicio07.java
+++ src/academy/devdojo/playlist03/Exercicio07.java
@@ -4,27 +4,35 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
-Elaborar um programa que calcule o fatorial de um número inteiro
+Melhorar o programa acima fazendo com que leia 5 números inteiros e calcule o
+fatorial dos 5 valores lidos.
  */
-public class Exercicio06 {
-    public static void main(String[] args) {
+public class Exercicio07 {
+    public static void main(String[] args){
 
+        int [] numeros = new int[5];
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Informe o número para calcular o fatorial:");
+        System.out.println("Informe 5 números para calcular o fatorial:");
         try {
-            int numero = scanner.nextInt();
-            int valor = numero;
+            for (int i = 0; i < numeros.length; i++) {
+                numeros[i] = scanner.nextInt();
 
-            while (numero > 1) {
-                valor = valor * (numero - 1);
-                numero--;
+                int valor = numeros[i];
+
+                while (numeros[i] > 1) {
+                    valor = valor * (numeros[i] - 1);
+                    numeros[i]--;
+
+                }System.out.println("Resultado do fatorial: "+valor);
+
+                }
+            } catch(InputMismatchException inputMismatchException){
+
+                System.out.println("Digite um valor numérico válido ");
             }
-            System.out.println(valor);
-        } catch (InputMismatchException e) {
-            System.out.println("Digite um valor numérico válido " + e.getLocalizedMessage());
-        }
+
     }
 }
 
