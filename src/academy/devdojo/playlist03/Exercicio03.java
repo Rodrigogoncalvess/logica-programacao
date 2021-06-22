f572c5f6ddd70009ad8cf327e56cc0ae2913efb1
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-22 18:52:16 -0300

    Exercício 03

--- src/academy/devdojo/playlist03/Exercicio03.java
+++ src/academy/devdojo/playlist03/Exercicio03.java
@@ -1,14 +1,20 @@
 package academy.devdojo.playlist03;
 
-import java.util.Scanner;
-
 /*
-Elaborar um programa que calcule e apresente a tabuada de um número qualquer.
+Elaborar um programa que apresente o somatório dos números na faixa de 1 até
+500.
  */
-public class Exercicio02 {
+public class Exercicio03 {
     public static void main(String[] args) {
 
+        int soma = 0;
 
+        System.out.print("A somatório do número 1 até 500 é: ");
+        for (int i = 1; i <= 500; i++)
+            soma += i;
+
+        System.out.println(soma);
     }
 }
+
 
