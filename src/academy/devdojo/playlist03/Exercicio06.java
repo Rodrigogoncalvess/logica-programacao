5af46b73226926d2436676257ec6739536fbad23
Rodrigo Gonçalves <rodrigo.goncalvesss@outlook.com>
2021-06-22 18:53:23 -0300

    Exercício 06

--- src/academy/devdojo/playlist03/Exercicio06.java
+++ src/academy/devdojo/playlist03/Exercicio06.java
@@ -1,23 +1,30 @@
 package academy.devdojo.playlist03;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
-Elaborar um programa que apresente a sequência de Fibonacci até o décimo quinto
-termo.
+Elaborar um programa que calcule o fatorial de um número inteiro
  */
-public class Exercicio05 {
+public class Exercicio06 {
     public static void main(String[] args) {
 
-        int num1 = 1;
-        int num2 = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 15; i++) {
-            num1 = num1 + num2;
-            num2 = num1 - num2;
-            System.out.println(num1);
+        System.out.println("Informe o número para calcular o fatorial:");
+        try {
+            int numero = scanner.nextInt();
+            int valor = numero;
 
+            while (numero > 1) {
+                valor = valor * (numero - 1);
+                numero--;
+            }
+            System.out.println(valor);
+        } catch (InputMismatchException i) {
+            System.out.println("Digite um valor numérico válido ");
         }
     }
-
 }
 
 
